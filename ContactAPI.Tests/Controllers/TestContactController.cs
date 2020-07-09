4e56036aa042c7fc5ae7f689ba4f18988dfe4174
priyankadhavale1 <priyankadhavale1@gmail.com>
2020-07-09 21:12:16 +0530

    Add project files.

---
+++ ContactAPI.Tests/Controllers/TestContactController.cs
@@ -0,0 +1,81 @@
+﻿
+using ContactAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ContactAPI.Controllers;
+
+namespace ContactAPI.Tests.Controllers
+{
+    [TestClass]
+    public class TestContactController
+    {
+        private ContactDetailsController _controller;
+
+        public TestContactController()
+        {
+            _controller = new ContactDetailsController();
+        }
+
+        [TestMethod]
+        public void GetAllContacts_ShouldReturnAllContacts()
+        {
+            var controller = new ContactDetailsController();
+
+            var result = _controller.GetContactDetails();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetContactById_ShouldReturnContact()
+        {
+            var controller = new ContactDetailsController();
+
+            var result = await _controller.GetContactDetail(4);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task AddEmployee_ShouldAddEmployee()
+        {
+            ContactDetail cd = new ContactDetail
+            {
+                FirstName = "Demo1",
+                LastName = "DemoLast1",
+                Email = "abc@abc.com",
+                PhoneNumber = "9876543210",
+                Status = true
+            };
+
+            var result = await _controller.PostContactDetail(cd);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task UpdateEmployee_ShouldUpdateEmployee()
+        {
+            ContactDetail cd = new ContactDetail
+            {
+                ID = 4,
+                FirstName = "Demo4",
+                LastName = "DemoLast4",
+                Email = "demolast4@abc.com",
+                PhoneNumber = "84375687435",
+                Status = true
+            };
+
+            var result = await _controller.PutContactDetail(cd);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task DeleteEmployee_ShouldDeleteEmployee()
+        {
+            var result = await _controller.DeleteContactDetail(4);
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ ContactAPI/Controllers/ContactDetailsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ContactAPI.Models;
+
+namespace ContactAPI.Controllers
+{
+    public class ContactDetailsController : ApiController
+    {
+        private ContactEntities db = new ContactEntities();
+
+        // GET: api/ContactDetails
+        public IQueryable<ContactDetail> GetContactDetails()
+        {
+            return db.ContactDetails;
+        }
+
+        // GET: api/ContactDetails/5
+        [ResponseType(typeof(ContactDetail))]
+        public async Task<IHttpActionResult> GetContactDetail(int id)
+        {
+            ContactDetail contactDetail = await db.ContactDetails.FindAsync(id);
+            if (contactDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contactDetail);
+        }
+
+        // PUT: api/ContactDetails/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutContactDetail(ContactDetail contactDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Entry(contactDetail).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContactDetailExists(contactDetail.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ContactDetails
+        [ResponseType(typeof(ContactDetail))]
+        public async Task<IHttpActionResult> PostContactDetail(ContactDetail contactDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ContactDetails.Add(contactDetail);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = contactDetail.ID }, contactDetail);
+        }
+
+        // DELETE: api/ContactDetails/5
+        [ResponseType(typeof(ContactDetail))]
+        public async Task<IHttpActionResult> DeleteContactDetail(int id)
+        {
+            ContactDetail contactDetail = await db.ContactDetails.FindAsync(id);
+            if (contactDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.ContactDetails.Remove(contactDetail);
+            await db.SaveChangesAsync();
+
+            return Ok(contactDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ContactDetailExists(int id)
+        {
+            return db.ContactDetails.Count(e => e.ID == id) > 0;
+        }
+    }
+}
